8584355f029c0d82f9593e7caf201c08cad44a64
Fogulus <fogulus@hotmail.fr>
2023-05-26 13:57:13 +0200

    live-5 fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Marque;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $marques = new Marque();
+        // $marques->setLibelle('KTM');
+        // $manager->persist($marques);
+
+        $marque1 = new Marque();
+        $marque1->setLibelle('KTM');
+        $manager->persist($marque1);
+
+        $marque2 = new Marque();
+        $marque2->setLibelle('Triumph');
+        $manager->persist($marque2);
+
+        $manager->flush();
+    }
+}
